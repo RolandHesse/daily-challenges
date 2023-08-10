dee10f48f1780451f5b43f742a16fed52605ea3d
Roland Hesse <rolandhesse11783@gmail.com>
2023-08-10 16:07:49 +0200

    solve js-unit-testing_get-animal

--- js-unit-testing_get-animal/index.js
+++ js-unit-testing_get-animal/index.js
@@ -1,1 +1,10 @@
-export function getAnimal() {}
+export function getAnimal(animalPluralName) {
+  if (animalPluralName === "cats") {
+    return `I totally love cats!`;
+  } else if (animalPluralName) {
+    return `I like ${animalPluralName}!`;
+  } else {
+    return "I do not like animals at all!";
+  }
+}
+//console.log(getAnimal("Discus fish", "Neon fish"));
