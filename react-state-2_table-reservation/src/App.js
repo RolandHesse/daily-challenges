b94af4d4655f8d03cf00d8722b8ade32310d6a92
Roland Hesse <rolandhesse11783@gmail.com>
2023-08-24 12:14:16 +0200

    solve react-state-2_table-reservation challenge

---
+++ react-state-2_table-reservation/src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Counter from "./components/Counter";
+import "./styles.css";
+import { useState } from "react";
+
+export default function App() {
+  const [people, setPeople] = useState(0);
+  function handleDecrease() {
+    setPeople(people - 1);
+  }
+  function handleIncrease() {
+    setPeople(people + 1);
+  }
+  return (
+    <div className="container">
+      <h1>Place a Table Reservation</h1>
+      <Counter onDecrease={handleDecrease} onIncrease={handleIncrease} />
+      <p>You are going to reserve a table for {people} people.</p>
+    </div>
+  );
+}
---
+++ react-state-2_table-reservation/src/components/Counter.js
@@ -0,0 +1,28 @@
+//import { useState } from "react";
+
+export default function Counter({ onDecrease, onIncrease }) {
+  //console.log("people: ", people);
+  return (
+    <>
+      <h2>How many people would you like to visit us with?</h2>
+      <div className="counter__buttons-container">
+        <button
+          type="button"
+          className="counter__button"
+          aria-label="increment people count"
+          onClick={onIncrease}
+        >
+          +
+        </button>
+        <button
+          type="button"
+          className="counter__button"
+          aria-label="decrement people count"
+          onClick={onDecrease}
+        >
+          -
+        </button>
+      </div>
+    </>
+  );
+}
