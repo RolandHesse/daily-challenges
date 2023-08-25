10869a4b64aba0de9044a7556cc8e54f2aa70574
Roland Hesse <rolandhesse11783@gmail.com>
2023-08-25 12:20:07 +0200

    solve react-state-3_tags challenge

---
+++ react-state-3_tags/src/App.js
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import "./styles.css";
+import Form from "./components/Form/index.js";
+import List from "./components/List/index.js";
+
+export default function App() {
+  const [tags, setTags] = useState(["JavaScript", "React", "CSS", "HTML"]);
+  console.log(tags);
+
+  function handleAddTag(newTag) {
+    setTags([...tags, newTag]);
+  }
+
+  function handleDeleteTag(tagToDelete) {
+    setTags(tags.filter((tag) => tagToDelete !== tag));
+  }
+
+  return (
+    <main className="app">
+      <Form onAddTag={handleAddTag} />
+      <List onDeleteTag={handleDeleteTag} tags={tags} />
+    </main>
+  );
+}
