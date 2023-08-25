6cc8ce1a9d9a7bc2a0f87969b2d5e2b8e35c82f6
Roland Hesse <rolandhesse11783@gmail.com>
2023-08-25 16:17:45 +0200

    solve react-state-3_journal-app-favorite-button-shared-state challenge

---
+++ react-state-3_journal-app-favorite-button-shared-state/src/components/FavoriteButton/index.js
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import "./FavoriteButton.css";
+import { ReactComponent as StarFilled } from "./star-filled.svg";
+import { ReactComponent as Star } from "./star.svg";
+
+export default function FavoriteButton({ onToggleFavorite, isFavorite, id }) {
+  function handleClick() {
+    onToggleFavorite(id);
+  }
+  return (
+    <button
+      className="favorite-button"
+      onClick={handleClick}
+      aria-label="favorite"
+    >
+      {isFavorite ? <StarFilled /> : <Star />}
+    </button>
+  );
+}
