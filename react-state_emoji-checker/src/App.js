5f85dcdb43ecc9f31be411d078fab8a8170f3f7a
Roland Hesse <rolandhesse11783@gmail.com>
2023-08-27 16:53:39 +0200

    solve react-state_emoji-checker challenge

---
+++ react-state_emoji-checker/src/App.js
@@ -0,0 +1,46 @@
+import React from "react";
+import "./styles.css";
+import { useState } from "react";
+
+export default function App() {
+  const [code, setCode] = useState("");
+
+  const validCode = "🐡🐠🐋";
+
+  function handleClick(event) {
+    setCode(code + event.target.textContent);
+  }
+
+  return (
+    <div className="container">
+      <div className="button-container">
+        <button type="button" onClick={handleClick}>
+          <span role="img" aria-label="Pufferfish">
+            🐡
+          </span>
+        </button>
+        <button type="button" onClick={handleClick}>
+          <span role="img" aria-label="Whale">
+            🐋
+          </span>
+        </button>
+        <button type="button" onClick={handleClick}>
+          <span role="img" aria-label="Clownfish">
+            🐠
+          </span>
+        </button>{" "}
+      </div>
+
+      <button
+        type="button"
+        onClick={() => {
+          setCode("");
+        }}
+      >
+        Reset
+      </button>
+      <p>Code entered: {code}</p>
+      {code === validCode && <p>Valid code!</p>}
+    </div>
+  );
+}
