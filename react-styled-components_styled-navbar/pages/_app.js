cae011e4027eecec1a1b3dcaa8f860aac3ad4074
Roland Hesse <rolandhesse11783@gmail.com>
2023-09-05 10:25:33 +0200

    solve react-styled-components-styled-navbar challenge

---
+++ react-styled-components_styled-navbar/pages/_app.js
@@ -0,0 +1,43 @@
+import Link from "next/link";
+import GlobalStyle from "../styles";
+import styled from "styled-components";
+
+const Nav = styled.nav`
+  border-bottom: solid 2px grey;
+  padding: 1rem;
+`;
+
+const StyledList = styled.ul`
+  list-style: none;
+  display: flex;
+  gap: 1rem;
+`;
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <GlobalStyle />
+      <Nav>
+        <StyledList>
+          <li>
+            <StyledLink href="/">Home</StyledLink>
+          </li>
+          <li>
+            <StyledLink href="/about">About</StyledLink>
+          </li>
+          <li>
+            <StyledLink href="/contact">Contact</StyledLink>
+          </li>
+        </StyledList>
+      </Nav>
+      <Component {...pageProps} />
+    </>
+  );
+}
