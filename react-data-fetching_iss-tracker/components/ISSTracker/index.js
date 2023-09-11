515a7be44ef2d06d519c98b2d499b579bf35fe1c
Roland Hesse <rolandhesse11783@gmail.com>
2023-09-11 11:42:03 +0200

    solve react-data-fetching_iss-tracker challenge

---
+++ react-data-fetching_iss-tracker/components/ISSTracker/index.js
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+import Controls from "../Controls/index";
+import Map from "../Map/index";
+import useSWR from "swr";
+
+const URL = "https://api.wheretheiss.at/v1/satellites/25544";
+
+const fetcher = async (url) => {
+  const res = await fetch(url);
+  if (!res.ok) {
+    const error = new Error("An error occurred while fetching the data.");
+    error.info = await res.json();
+    error.status = res.status;
+    throw error;
+  }
+
+  return res.json();
+};
+
+export default function ISSTracker() {
+  const { data, isLoading, error, mutate } = useSWR(URL, fetcher, {
+    refreshInternval: 5000,
+  });
+
+  if (isLoading) return <div>loading...</div>;
+  if (error) return <div>failed to load</div>;
+
+  return (
+    <main>
+      <Map longitude={data.longitude} latitude={data.latitude} />
+      <Controls
+        longitude={data.longitude}
+        latitude={data.latitude}
+        onRefresh={() => mutate()}
+      />
+    </main>
+  );
+}
