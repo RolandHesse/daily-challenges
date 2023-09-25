1a77272bed1cf077697f6919b2acdeaa79b71c23
Roland Hesse <rolandhesse11783@gmail.com>
2023-09-25 12:16:11 +0200

    solve backend-update-and-delete_products challenge

--- backend-update-and-delete_products/components/Product/index.js
+++ backend-update-and-delete_products/components/Product/index.js
@@ -3,12 +3,43 @@ import { useRouter } from "next/router";
 import { ProductCard } from "./Product.styled";
 import { StyledLink } from "../Link/Link.styled";
 import Comments from "../Comments";
+import { useState } from "react";
+import ProductForm from "../ProductForm";
 
 export default function Product() {
   const router = useRouter();
   const { id } = router.query;
+  const [isEditMode, setIsEditMode] = useState(false);
 
-  const { data, isLoading } = useSWR(`/api/products/${id}`);
+  const { data, isLoading, mutate } = useSWR(`/api/products/${id}`);
+
+  async function handleEditProduct(event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const productData = Object.fromEntries(formData);
+
+    const response = await fetch(`/api/products/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(productData),
+    });
+
+    if (response.ok) {
+      mutate();
+      setIsEditMode(false);
+    }
+  }
+
+  async function handleDelete() {
+    await fetch(`/api/products/${id}`, {
+      method: "DELETE",
+    });
+
+    router.push("/");
+  }
 
   if (isLoading) {
     return <h1>Loading...</h1>;
@@ -26,6 +57,27 @@ export default function Product() {
         Price: {data.price} {data.currency}
       </p>
       {data.reviews.length > 0 && <Comments reviews={data.reviews} />}
+      <div>
+        <button
+          onClick={() => {
+            setIsEditMode(!isEditMode);
+          }}
+        >
+          Edit Product
+        </button>
+        <button onClick={handleDelete} disabled={isEditMode}>
+          Delete product
+        </button>
+      </div>
+
+      {isEditMode && (
+        <ProductForm
+          onSubmit={handleEditProduct}
+          value={data}
+          editMode={true}
+        />
+      )}
+      <br />
       <StyledLink href="/">Back to all</StyledLink>
     </ProductCard>
   );
--- backend-update-and-delete_products/pages/index.js
+++ backend-update-and-delete_products/pages/index.js
@@ -1,6 +1,7 @@
 import ProductList from "../components/ProductList";
 import styled from "styled-components";
 import ProductForm from "../components/ProductForm";
+import useSWR from "swr";
 
 const Heading = styled.h1`
   text-align: center;
@@ -8,6 +9,33 @@ const Heading = styled.h1`
 `;
 
 export default function HomePage() {
+  const { mutate } = useSWR("/api/products");
+
+  async function handleAddProduct(event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const productData = Object.fromEntries(formData);
+
+    const response = await fetch("/api/products", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(productData),
+    });
+
+    if (!response.ok) {
+      console.error(response.status);
+      return;
+    }
+
+    mutate();
+    const responseData = await response.json();
+    alert(responseData.status);
+    event.target.reset();
+  }
+
   return (
     <>
       <Heading>
@@ -16,8 +44,7 @@ export default function HomePage() {
         </span>
         Fish Shop
       </Heading>
-      <ProductForm />
-      <hr />
+      <ProductForm onSubmit={handleAddProduct} editMode={false} />
       <ProductList />
     </>
   );
